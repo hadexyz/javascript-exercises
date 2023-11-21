401779bd8750b125fd85de8d6c876c1a0abc4161
hadexyz <142442544+hadexyz@users.noreply.github.com>
2023-11-21 14:19:29 -0300

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str){
+    toString(str);
+    let finalStr = ""
+    for (let i=1;i <= str.length; i++) {
+        let lastLetter = str.charAt(str.length - i);
+        finalStr += lastLetter
+    } 
+    return finalStr
 };
 
 // Do not edit below this line
