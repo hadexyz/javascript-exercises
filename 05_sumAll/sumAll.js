a72abf90613968e667c95c98c0dc7e2571f0ab21
hadexyz <142442544+hadexyz@users.noreply.github.com>
2023-12-07 15:22:52 -0300

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function (x, y) {
+    if (x < 0 || y < 0) {return "ERROR"}
+    if (typeof x != "number" || typeof y != "number") {return "ERROR"}
+    let sum = 0
+    // this if statement in case x is bigger than y
+    if (x > 0 && x > y){
+        for (let i = y; i <= x; i++) {
+            sum += i
+        }
+    }
+    if (y > 0 && y > x) {
+       for (let i = x; i <= y; i++) {
+            sum += i
+        } 
+    }
+    return sum
 };
 
 // Do not edit below this line
