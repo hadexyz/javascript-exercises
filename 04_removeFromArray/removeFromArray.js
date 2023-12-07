88fe08546d0928cee0e63a8b699dfa0d1a26d56f
hadexyz <142442544+hadexyz@users.noreply.github.com>
2023-12-07 14:29:47 -0300

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...parameters) {
+   let finalarr = arr
+   for (const arg of parameters) {
+    const index = finalarr.indexOf(arg)
+    if (index != -1 ) {
+        arr.splice(index,1)
+    }
+   } 
+   return finalarr
 };
 
 // Do not edit below this line
